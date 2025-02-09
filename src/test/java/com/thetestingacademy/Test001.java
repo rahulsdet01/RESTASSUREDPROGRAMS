9a716fa8daa4b0979a804f2c3fb5e613ef3b4afa
RAHUL NEGI <rahul.sdet01@gmail.com>
2025-02-09 14:01:17 +0530

    push code

--- src/test/java/com/thetestingacademy/Test001.java
+++ src/test/java/com/thetestingacademy/Test001.java
@@ -2,5 +2,24 @@ package com.thetestingacademy;
 import io.restassured.RestAssured;
 
 public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Rest Assured Test Case");
+        System.out.println("GET Request Demo");
+
+        //     Gherkins Syntax
+
+        //     given() - url,headers,body or payload
+        //     When() - http methods - get, post,patch , put ,delete.
+        //     then() - verify the response
+
+        RestAssured
+                .given()
+                        .baseUri("https://restful-booker.herokuapp.com")
+                        .basePath("/booking/1").log().all()
+                .when()
+                        .get()
+                .then().log().all()
+                        .statusCode(200);
+    }
 
 }
